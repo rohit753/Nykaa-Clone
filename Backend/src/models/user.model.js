2833b5fe644524e8cc8fc45ffa167fa3136b91ad
Sourabh Hupare <83506785+sourabh-s1@users.noreply.github.com>
2022-01-19 13:59:23 +0530

    Backend Added

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required:true, unique: true},
+    profile: {type: String, required: true},
+    role: {type: String, required: true, default: 'customer'}
+},
+{
+    timestamps: true,
+    versionKey: false,
+})
+
+module.exports = mongoose.model('user', userSchema);
